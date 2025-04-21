23449438e2da7e83975b3486927491dd185f7c13
Grace Yishak <gyishak@westmont.edu>
2025-04-21 02:37:50 -0700

    Finished Lab

---
+++ cs150-lab5/Chapter-7/dash-svm/app.py
@@ -0,0 +1,250 @@
+import time
+import importlib
+
+import pandas as pd
+from dash import Dash, dcc, html, Input, Output, State
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn import datasets
+from sklearn.svm import SVC
+
+import utils.dash_reusable_components as drc
+import utils.figures as figs
+
+app = Dash(
+    __name__,
+    meta_tags=[
+        {"name": "viewport", "content": "width=device-width, initial-scale=1.0"}
+    ],
+)
+app.title = "Diabetes Stats Based on Age and Glucose Levels"
+server = app.server
+
+def generate_data():
+    df = pd.read_csv("assets/diabetes.csv")
+
+    X = df[['Glucose', 'Age']]
+    y = df['Outcome']
+    return X
+
+def generate_outcome():
+    df = pd.read_csv("assets/diabetes.csv")
+
+
+    y = df['Outcome']
+    return y
+
+app.layout = html.Div(
+    children=[
+        html.Div(
+            className="banner",
+            children=[
+                # Change App Name here
+                html.Div(
+                    className="container scalable",
+                    children=[
+                        # Change App Name here
+                        html.H2(
+                            id="banner-title",
+                            children=[
+                                html.A(
+                                    "Diabetes Stats Based on Age and Glucose Levels",
+                                    href="https://github.com/plotly/dash-svm",
+                                    style={
+                                        "text-decoration": "none",
+                                        "color": "inherit",
+                                    },
+                                )
+                            ],
+                        ),
+                        html.A(
+                            id="banner-logo",
+                            children=[
+                                html.Img(src=app.get_asset_url("dash-logo-new.png"))
+                            ],
+                            href="https://plot.ly/products/dash/",
+                        ),
+                    ],
+                )
+            ],
+        ),
+        html.Div(
+            id="body",
+            className="container scalable",
+            children=[
+                html.Div(
+                    id="app-container",
+                    children=[
+                        html.Div(
+                            id="left-column",
+                            children=[
+                                drc.Card(
+                                    id="button-card",
+                                    children=[
+                                        drc.NamedSlider(
+                                            name="Threshold",
+                                            id="slider-threshold",
+                                            min=0,
+                                            max=1,
+                                            value=0.5,
+                                            step=0.01,
+                                        ),
+                                        html.Button(
+                                            "Reset Threshold",
+                                            id="button-zero-threshold",
+                                        ),
+                                    ],
+                                ),
+                                drc.Card(
+                                    id="last-card",
+                                    children=[
+                                        drc.NamedDropdown(
+                                            name="Kernel",
+                                            id="dropdown-svm-parameter-kernel",
+                                            options=[
+                                                {
+                                                    "label": "Radial basis function (RBF)",
+                                                    "value": "rbf",
+                                                },
+                                                {"label": "Linear", "value": "linear"},
+                                                {
+                                                    "label": "Polynomial",
+                                                    "value": "poly",
+                                                },
+                                                {
+                                                    "label": "Sigmoid",
+                                                    "value": "sigmoid",
+                                                },
+                                            ],
+                                            value="rbf",
+                                            clearable=False,
+                                            searchable=False,
+                                        ),
+                                    ],
+                                ),
+                            ],
+                        ),
+                        html.Div(
+                            id="div-graphs",
+                            children=dcc.Graph(
+                                id="graph-sklearn-svm",
+                                figure=dict(
+                                    layout=dict(
+                                        plot_bgcolor="#282b38", paper_bgcolor="#282b38"
+                                    )
+                                ),
+                            ),
+                        ),
+                    ],
+                )
+            ],
+        ),
+    ]
+)
+
+
+@app.callback(
+    Output("slider-threshold", "value"),
+    [Input("button-zero-threshold", "n_clicks")],
+    [State("graph-sklearn-svm", "figure")],
+)
+def reset_threshold_center(n_clicks, figure):
+    if n_clicks:
+        Z = np.array(figure["data"][0]["z"])
+        value = -Z.min() / (Z.max() - Z.min())
+    else:
+        value = 0.4959986285375595
+    return value
+
+
+@app.callback(
+    Output("div-graphs", "children"),
+    [
+        Input("dropdown-svm-parameter-kernel", "value"),
+        #Input("dropdown-select-dataset", "value"),
+        Input("slider-threshold", "value"),
+    ],
+)
+def update_svm_graph(
+    kernel,
+    threshold,
+
+):
+    t_start = time.time()
+    h = 0.3
+
+    # Data Pre-processing
+    X= generate_data()
+    y= generate_outcome()
+    X = StandardScaler().fit_transform(X)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.4, random_state=42
+    )
+
+    x_min = X[:, 0].min() - 0.5
+    x_max = X[:, 0].max() + 0.5
+    y_min = X[:, 1].min() - 0.5
+    y_max = X[:, 1].max() + 0.5
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+
+
+
+    clf = SVC(kernel=kernel)
+    clf.fit(X_train, y_train)
+
+
+    if hasattr(clf, "decision_function"):
+        Z = clf.decision_function(np.c_[xx.ravel(), yy.ravel()])
+    else:
+        Z = clf.predict_proba(np.c_[xx.ravel(), yy.ravel()])[:, 1]
+
+    prediction_figure = figs.serve_prediction_plot(
+        model=clf,
+        X_train=X_train,
+        X_test=X_test,
+        y_train=y_train,
+        y_test=y_test,
+        Z=Z,
+        xx=xx,
+        yy=yy,
+        mesh_step=h,
+        threshold=threshold,
+    )
+
+    roc_figure = figs.serve_roc_curve(model=clf, X_test=X_test, y_test=y_test)
+
+    confusion_figure = figs.serve_pie_confusion_matrix(
+        model=clf, X_test=X_test, y_test=y_test, Z=Z, threshold=threshold
+    )
+
+    return [
+        html.Div(
+            id="svm-graph-container",
+            children=dcc.Loading(
+                className="graph-wrapper",
+                children=dcc.Graph(id="graph-sklearn-svm", figure=prediction_figure),
+                style={"display": "none"},
+            ),
+        ),
+        html.Div(
+            id="graphs-container",
+            children=[
+                dcc.Loading(
+                    className="graph-wrapper",
+                    children=dcc.Graph(id="graph-line-roc-curve", figure=roc_figure),
+                ),
+                dcc.Loading(
+                    className="graph-wrapper",
+                    children=html.Div(
+                        id="graph-pie-confusion-matrix", children=confusion_figure
+                    ),
+                ),
+            ],
+        ),
+    ]
+
+
+# Running the server
+if __name__ == "__main__":
+    app.run_server(debug=True)
